4b09703b30716d54fc39bee3489209d807352f26
juched <prth.2000@gmail.com>
2022-02-08 16:18:00 -0500

    webRTC: Added webrtc code. Still doesn't compiled

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3
 ADD . /code
 WORKDIR /code
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt \
+    && apt update && apt install -y python3-numpy python3-cffi python3-aiohttp \
+    libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev \
+    libswscale-dev libswresample-dev libavfilter-dev libopus-dev \
+    libvpx-dev pkg-config libsrtp2-dev python3-opencv pulseaudio
 CMD python app.py
--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 # compose_flask/app.py
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis
+
+from rtcbot import RTCConnection, getRTCBotJS
+
+conn = RTCConnection()  # For this example, we use just one global connection
+
+@conn.subscribe
+def onMessage(msg):  # Called when messages received from browser
+    print("Got message:", msg["data"])
+    conn.put_nowait({"data": "pong"})
+
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+# This sets up the connection
+@app.route("/connect", methods = ['POST'])
+async def connect(request):
+    clientOffer = await request.json()
+    serverResponse = await conn.getLocalDescription(clientOffer)
+    return web.json_response(serverResponse)
+
+
+@app.route('/rtcbot.js')
+def rtcbotjs(request):
+    return getRTCBotJS()
+
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return f"This Compose/Flask demo has been viewed {int(redis.get('hits'))} time(s)."
+    return render_template("mockup.html")
+
+async def cleanup(app=None):
+    await conn.close()
 
 
 if __name__ == "__main__":
