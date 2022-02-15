b5a56deefa6307fa5403651cc0b05b43cb5a2df2
whitemanthedj <awhiteman2000@gmail.com>
2022-02-15 16:45:05 -0500

    adding prelim python logic for requesting songs

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template
 
 from flask_sock import Sock
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 sock = Sock(app)
@@ -16,6 +18,12 @@ def echo(sock):
     while True:
         data = sock.receive()
         sock.send(data)
+
+
+@app.route('/player')
+def player():
+    return f""
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
