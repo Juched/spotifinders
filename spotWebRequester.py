8a01ac08b592b039e46313d77073dcdcc00f9ff1
whitemanthedj <awhiteman2000@gmail.com>
2022-02-16 11:44:15 -0500

    preliminary untested code for playing a song from spotify

--- spotWebRequester.py
+++ spotWebRequester.py
@@ -9,3 +9,6 @@ def requestSong():
     # results = sp.search(q='artist:' + name, type='artist')
     # items = results['artists']['items']
     # sp.recommendations(seed_genres=recommendation_genre_seeds())
+    # q=name:abacab&type=album,track where q is the query and type is the thing with that name, returns list of album or tracks in this case with name q
+
+    
