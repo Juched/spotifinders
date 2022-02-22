058b1a635a3ce9a1586e2017403a435b015ef1d8
sade2 <cedricsman@gmail.com>
2022-02-22 17:50:42 -0500

    Webplayer now hijacks your spotify playing. Refresh the page will play a hardcoded song.

--- static/web_player.js
+++ static/web_player.js
@@ -5,56 +5,83 @@ if (window.location.protocol == "https:") {
   var ws_scheme = "ws://";
 }
 
-const webPlayerSocket = new WebSocket(ws_scheme + location.host + '/webPlayer');
+// This function establishes a websocket and waits for it to get a message (the auth token)
+function connect() {
+  return new Promise(function(resolve, reject) {
+      var server = new WebSocket(ws_scheme + location.host + '/webPlayer');
+      server.onmessage = function(event) {
+
+          resolve(event.data);
+      };
+      server.onerror = function(err) {
+          reject(err);
+      };
+
+  });
+}
 
 
-var authTok = null;
-
-// Spotify Web Player won't get its "onReady" methods set until the authToken is sent from the backend.
-
-webPlayerSocket.addEventListener('message', ev => {
-// Once we get a message (auth token), establish SDK
-  console.log(ev.data)
-  authTok = ev.data
-});
 
 
 
 window.onSpotifyWebPlaybackSDKReady = () => {
-  while (authTok == null) {
-    console.log('awaiting authtok')
 
-  } //await
-  console.log("obtained token: " + authTok)
+  // This is where the function is called
+  connect().then(function(socketMsg) {
 
-  const token = authTok; //Auth token!
-  const player = new Spotify.Player({
-    name: 'Spotifinders Web Player',
-    getOAuthToken: cb => { cb(token); },
-    volume: 0.5
-  });
-  // Ready
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id);
-  });
+    authTok = socketMsg
+    console.log("Obtained authtok "  + authTok)
 
-  // Not Ready
-  player.addListener('not_ready', ({ device_id }) => {
-    console.log('Device ID has gone offline', device_id);
-  });
-  player.addListener('initialization_error', ({ message }) => { 
-    console.error(message);
-  });
+    const token = authTok; //Auth token!
+    console.log("Creating new player with token " + authTok)
+    const player = new Spotify.Player({
+      name: 'Spotifinders Web Player',
+      getOAuthToken: cb => { cb(token); },
+      volume: 0.5
+    });
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+      const deviceServer = new WebSocket(ws_scheme + location.host + '/deviceID');
+      deviceServer.addEventListener('open', (event) => {
+        deviceServer.send(device_id);
+        console.log('sent device id to client ' + device_id)
+      });
 
-  player.addListener('authentication_error', ({ message }) => {
+    });
+  
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+    player.addListener('initialization_error', ({ message }) => { 
       console.error(message);
-  });
+    });
+  
+    player.addListener('authentication_error', ({ message }) => {
+        console.error(message);
+    });
+  
+    player.addListener('account_error', ({ message }) => {
+        console.error(message);
+    });
+    document.getElementById('start_device').onclick = function() {
+      player.activateElement();
+      player.togglePlay();
+    };
+    player.connect().then(success => {
+      if (success) {
+        console.log('The Web Playback SDK successfully connected to Spotify!');
+      }
+    });
 
-  player.addListener('account_error', ({ message }) => {
-      console.error(message);
+  }).catch(function(err) {
+    // error here
+    console.log("failed to get authtok. No player created")
   });
+  
 
-  player.connect();
+
 }
 
 // var that = this;
