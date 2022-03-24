8f0c0a3ea364db170ea6523bb949eae187e1b07c
whitemanthedj <awhiteman2000@gmail.com>
2022-03-24 01:40:44 -0400

    incorrect import stuff corrected

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def get_spotipy():
 
         # standard
         cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
-        auth_manager = SpotifyClientCredentials.SpotifyOAuth(cache_handler=cache_handler)
+        auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
         if not auth_manager.validate_token(cache_handler.get_cached_token()):
             # return redirect('/')
             raise Exception('Was unable to connct to your spotify account')
