23894686083efe2e7be0b192cd51c9d3625744cc
Juched <prth.2000@gmail.com>
2022-03-30 20:31:54 -0400

    Added config to bert

--- models/bert_model.py
+++ models/bert_model.py
@@ -21,15 +21,8 @@ class BERTModel(NLPModel):
             A BERT model
         """
         with open("../cfg/config.yaml", "r") as f:
-            config = yaml.load(f, Loader=Loader)
-        self.ecstacy = config["ecstacy"]
-        self.admiration = config["admiration"]
-        self.terror = config["terror"]
-        self.amazement = config["amazement"]
-        self.grief = config["grief"]
-        self.loathing = config["loathing"]
-        self.rage = config["rage"]
-        self.vigilance = config["vigilance"]
+            self.config = yaml.load(f, Loader=Loader)
+
         self.model = torch.load("bin/bert.mod")
         self.tokenizer = BertTokenizer.from_pretrained(
             "bert-base-uncased", do_lower_case=True
@@ -60,5 +53,59 @@ class BERTModel(NLPModel):
         """
         tok_input = self.tokenizer(text, padding=True, max_length=100)
         tok_input = self.convert_to_t_n(tok_input)
+        classes = self.model(tok_input["input_ids"], tok_input["attention_mask"])
+        feature_dict = {"danceability": 0.0, "energy": 0.0, "valence": 0.0}
+        classes = classes.tolist()
+        
+        #clamp so we don't get weird nums
+        for c in classes:
+            if c < 0:
+                c = 0.0
+            elif c > 1:
+                c = 1.0
 
-        return self.model(tok_input["input_ids"], tok_input["attention_mask"])
+            c = c - 0.5
+            c = c * 2
+
+        #apply configs
+        feature_dict["danceability"] = (
+            (self.config["d"]["e"] * classes[0])
+            + (self.config["d"]["ad"] * classes[1])
+            + (self.config["d"]["t"] * classes[2])
+            + (self.config["d"]["am"] * classes[3])
+            + (self.config["d"]["g"] * classes[4])
+            + (self.config["d"]["l"] * classes[5])
+            + (self.config["d"]["r"] * classes[6])
+            + (self.config["d"]["v"] * classes[7])
+        )
+
+        feature_dict["energy"] = (
+            (self.config["e"]["e"] * classes[0])
+            + (self.config["e"]["ad"] * classes[1])
+            + (self.config["e"]["t"] * classes[2])
+            + (self.config["e"]["am"] * classes[3])
+            + (self.config["e"]["g"] * classes[4])
+            + (self.config["e"]["l"] * classes[5])
+            + (self.config["e"]["r"] * classes[6])
+            + (self.config["e"]["v"] * classes[7])
+        )
+
+        feature_dict["valence"] = (
+            (self.config["v"]["e"] * classes[0])
+            + (self.config["v"]["ad"] * classes[1])
+            + (self.config["v"]["t"] * classes[2])
+            + (self.config["v"]["am"] * classes[3])
+            + (self.config["v"]["g"] * classes[4])
+            + (self.config["v"]["l"] * classes[5])
+            + (self.config["v"]["r"] * classes[6])
+            + (self.config["v"]["v"] * classes[7])
+        )
+
+        # clamp
+        for k, v in feature_dict.items():
+            if v < 0:
+                v = 0
+            elif v > 1:
+                v = 1
+
+        return feature_dict
