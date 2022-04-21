caa55a13b9e18d9a96dd8cc1a2f76e92d8e5060e
sade2 <cedricsman@gmail.com>
2022-04-20 23:13:36 -0400

    Quick miniscule fixes

--- static/loggedin.js
+++ static/loggedin.js
@@ -18,6 +18,8 @@ function submitTextInBox()
 
   let data = {text: text_box_data};
 
+  closeMenu();
+
   fetch("http://localhost:5000/book", {
      method: "POST",
      headers: {'Content-Type': 'application/json'},
@@ -29,4 +31,6 @@ function submitTextInBox()
   }).catch(error => {
      console.log(error);
   });
+
+
 }
